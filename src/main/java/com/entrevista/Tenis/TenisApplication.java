c75265614c9810d3e81576823bfe267419711282
FCalvo931 <franfran304@gmail.com>
2022-10-26 08:42:23 -0300

    Cambios
    Se cambio el Pom

--- src/main/java/com/entrevista/Tenis/TenisApplication.java
+++ src/main/java/com/entrevista/Tenis/TenisApplication.java
@@ -4,19 +4,14 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.List;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import com.entrevista.Tenis.Service.JugadorService;
 import com.entrevista.Tenis.entity.Jugador;
 import com.entrevista.Tenis.entity.Partido;
 
-@SpringBootApplication
 public class TenisApplication {
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		SpringApplication.run(TenisApplication.class, args);
 		JugadorService js = new JugadorService();
 		String nombre1 = "";
 		int probabilidad1 = 0;
